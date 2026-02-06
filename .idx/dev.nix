b6a6cd3f738a088b43923fbdd69314c5f30e1c7a
Carlos Polaco <polaco85@gmail.com>
2026-02-06 19:41:38 -0300

    fix: minimal dev.nix config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,32 +1,5 @@
 { pkgs, ... }: {
   channel = "stable-23.11";
-
-  packages = [
-    pkgs.nodejs_18
-    pkgs.jdk17
-    pkgs.gradle
-  ];
-
-  env = {
-    JAVA_HOME = "${pkgs.jdk17}";
-  };
-
-  idx = {
-    extensions = [
-      "msjsdiag.vscode-react-native"
-      "dbaeumer.vscode-eslint"
-      "esbenp.prettier-vscode"
-    ];
-
-    workspace = {
-      onCreate = {
-        npm-install = "npm install";
-      };
-    };
-
-    previews = {
-      enable = true;
-      previews = {};
-    };
-  };
+  packages = [ pkgs.nodejs_18 ];
+  idx.workspace.onCreate.install = "npm install";
 }
